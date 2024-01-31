c175601a67f20b0d93a393c679881953f1a3b794
Matt Jones <gitcode@magisa.org>
2024-01-31 11:28:48 -0900

    Basic pytest framework setup.

---
+++ tests/test_loaddata.py
@@ -0,0 +1,12 @@
+from pandas import DataFrame
+
+def test_init():
+    """Initialize tests and show they are working.
+    """
+    assert 1 == 1
+    
+def test_load_data():
+    """Load example testing data for staging tests.
+    """
+    df = DataFrame( dict( x=[1,2,3], y=[4,5,6] ) )
+    assert df is not None
